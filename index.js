41f5caa7afeddb890d5b667a7f5128e0b0a39365
BANIKKURAISHI <mdkuraishirahmanbanik@gmail.com>
2023-11-27 15:40:39 +0600

    I make a admin and install stripejs

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const cors = require('cors')
 const jwt = require("jsonwebtoken");
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config()
+const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
 const port =process.env.PORT||5000
 const app = express()
 app.use(express.json())
@@ -50,11 +51,59 @@ const verifyToken=(req,res,next)=>{
   })
 }
 
+app.get("/user/admin/:email", verifyToken, async (req, res) => {
+      const email = req.params.email;
+      if (!email === req.decoded.email) {
+        return res.status(403).send({ message: "unauthorized access " });
+      }
+      const query = { email: email };
+      const result = await userCollection.findOne(query);
+      let admin = false;
+      if (result) {
+        admin = result?.role === "admin";
+      }
+      res.send({ admin });
+    });
+
+
+const verifyAdmin=async(req,res,next)=>{
+  const email = req.decoded.email;
+  const query = { email: email };
+  const user =await userCollection.findOne(query)
+  const admin=user?.role==="admin"
+  if(admin){
+    return res.status(403).send({ message: "unauthorized access " });
+  }
+  next()
+
+}
+
 
 
 
 
 ///user collection 
+app.get('/organizer-profile',async(req,res)=>{
+ 
+  const result =await userCollection.find().toArray()
+  res.send(result)
+  //console.log(result)
+})
+
+
+app.patch('/organizer-profile/admin/:id',verifyToken,async(req,res)=>{
+  const id=req.params.id 
+  const query ={_id:new ObjectId(id)}
+  const update={
+    $set:{
+      role:"admin"
+    }
+  }
+  const result =await userCollection.updateOne(query,update)
+  res.send(result)
+})
+
+
 app.post('/user',async(req,res)=>{
   const user = req.body;
   const query ={email:user.email}
@@ -75,6 +124,10 @@ app.post('/registration',async(req,res)=>{
   res.send(result)
  // console.log(result)
 })
+
+
+
+
 app.get('/registration',async(req,res)=>{
   const email=req?.query?.email 
   console.log(email)
@@ -101,7 +154,7 @@ app.get('/registration/:id',async(req,res)=>{
 app.get('/bestCamps',async(req,res)=>{
   // const filter =req.query 
   // const query={full_description:}
-   const result =await adminAddCollection.find().sort({ attendance:-1}).limit(2).toArray()
+   const result =await adminAddCollection.find().sort({ attendance:-1}).limit(6).toArray()
    res.send(result)
  
  })
